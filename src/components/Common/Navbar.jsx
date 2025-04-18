85426d86472db0b760c138ca3b4665bc2d061b18
Eswar Lellaboina <eswar@Eswars-MacBook-Air.local>
2025-04-19 00:44:41 +0530

    Added company name

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -52,8 +52,7 @@ function Navbar() {
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link to="/">
-          {/* <div>Eswar</div> */}
-          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+          <h5 className="text-yellow-25">Thrive Evolve</h5>
         </Link>
         {/* Navigation links */}
         <nav className="hidden md:block">
